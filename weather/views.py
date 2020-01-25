baf2553cf4ddb755a88074103d473a49f1873e24
Nathan Kuo <nathan.kuo.bcit@gmail.com>
2020-01-25 21:40:55 +0100

    Add add function

--- weather/views.py
+++ weather/views.py
@@ -1,14 +1,18 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=f4624920362748e4918830360342e0fc'
-    city = 'Sarajevo'
-
     cities = City.objects.all()
-
     weather_data = []
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
 
     for city in cities:
         response = requests.get(url.format(city)).json()
@@ -23,7 +27,7 @@ def index(request):
         weather_data.append(city_weather)
 
 
-    context = {'weather_data': weather_data}
+    context = {'weather_data': weather_data, 'form' : form }
 
     return render(request, 'weather/weather.html', context)
 # Create your views here.
